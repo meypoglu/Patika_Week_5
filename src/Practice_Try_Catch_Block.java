2797af9761c09bc737cd20a2cba5a24a7ef33430
meyup <m.eypoglu@gmail.com>
2024-05-21 15:18:40 +0300

    update

--- src/Practice_Try_Catch_Block.java
+++ src/Practice_Try_Catch_Block.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Practice_Try_Catch_Block {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in); // Kullanıcıdan girdi alınır
         System.out.println("Bir değer giriniz: ");
         int num = inp.nextInt();
-        returnByIndex(num);
+        returnByIndex(num); // Metod çağrılır
     }
 
     static void returnByIndex(int inx) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Oluşturulan array
         try {
-            System.out.println(array[inx]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(e.getClass().getName() + ": İndeks array boyutları dışındadır.");
+            System.out.println(array[inx]); // Arraydeki belirtilen indeks yazdırılmaya çalışılır, eğer hata alınırsa catch bloğuna girilir.
+        } catch (ArrayIndexOutOfBoundsException e) { // OutOfBounds hatası burda yakalanır
+            System.out.println(e.getClass().getName() + ": İndeks array boyutları dışındadır."); // Hata sınıfı alınır ve hata mesajıyla beraber yazdırılır
         }
     }
 }
