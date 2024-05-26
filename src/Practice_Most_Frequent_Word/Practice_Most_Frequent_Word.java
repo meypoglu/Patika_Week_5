0fdff4d572e40787014a7f68db9698bdf238389b
meyup <m.eypoglu@gmail.com>
2024-05-26 08:50:33 +0300

    update

--- src/Practice_Most_Frequent_Word/Practice_Most_Frequent_Word.java
+++ src/Practice_Most_Frequent_Word/Practice_Most_Frequent_Word.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class Practice_Most_Frequent_Word {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in); // Kullanıcıdan girdinin alınması
         System.out.println("Metni giriniz: ");
         String text = inp.nextLine();
 
-        String[] splitText = text.split("\\s+");
-        HashMap<String,Integer> words = new HashMap<String,Integer>();
+        String[] splitText = text.split("\\s+"); // Alınan girdi split metodu ile kelimelere ayrılır
+        HashMap<String,Integer> words = new HashMap<String,Integer>(); // Kelimelerin kaç kere tekrar edildiğinin tespit edilebilmesi için HashMap oluşturulur
 
-        for (String k : splitText) {
+        for (String k : splitText) { // Ayrılan kelimeler HashMap'e kaydedilir. Eğer halihazırda kayıtlı bir kelime varsa value kısmı 1 artırılır
             if (words.containsKey(k)) {
                 words.put(k, words.get(k) + 1);
             } else {
@@ -19,10 +19,10 @@ public class Practice_Most_Frequent_Word {
             }
         }
 
-        Iterator<Map.Entry<String, Integer>> hashIt = words.entrySet().iterator();
+        Iterator<Map.Entry<String, Integer>> hashIt = words.entrySet().iterator(); // Elde edilen HashMap'teki en çok değere sahip key bir Iterator yardımıyla tespit edilir
 
-        int maxCounter = 0;
-        String maxKey = null;
+        int maxCounter = 0; // Maksimum değeri tutan değişken
+        String maxKey = null; // Maksimum değere sahip key'i tutan değişken
         while(hashIt.hasNext()) {
             Map.Entry<String, Integer> key = hashIt.next();
             if (key.getValue() > maxCounter) {
